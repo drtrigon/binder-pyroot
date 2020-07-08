69f7d717977614838804aa478c37c943daf86f0e
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-07-08 11:53:05 +0200

    add notebook about jitting and dynamic c++

---
+++ analysis.cxx
@@ -0,0 +1,12 @@
+# include "analysis.hxx"
+
+float optimized_largest_sum(float* v1, float* v2, std::size_t size){
+    float r = -999.f;
+    for (size_t i1 = 0; i1 < size; i1++) {
+        for (size_t i2 = 0; i2 < size; i2++) {
+            const auto tmp = v1[i1] + v2[i2];
+            if (tmp > r) r = tmp;
+        }
+    }
+    return r;
+}
---
+++ analysis.hxx
@@ -0,0 +1,2 @@
+#include <cstddef>
+float optimized_largest_sum(float* v1, float* v2, std::size_t size);
